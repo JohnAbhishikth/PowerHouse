df8a7fa4542e7bf8d78dec0cb35380e47e24eb8a
JohnAbhishikth <kjabhishikth@gmail.com>
2021-10-27 20:45:23 +0530

    admin changes

--- PowerHouse/src/app/admin/update-merchant/update-merchant.component.ts
+++ PowerHouse/src/app/admin/update-merchant/update-merchant.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { MerchantService } from 'src/services/merchant.service';
 import { MerchantDTO } from 'src/app/dto/MerchantDTO';
+import { LoginService } from 'src/services/login.service';
+import { Router } from '@angular/router';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-update-merchant',
@@ -13,12 +16,33 @@ export class UpdateMerchantComponent implements OnInit {
   merchants: MerchantDTO[] = []
   errorMsg!: string
   buttons: any
-  constructor(private merchantService: MerchantService) { }
+  userName: string;
 
-  ngOnInit(): void { }
+  constructor(private merchantService: MerchantService, private loginService: LoginService, private router: Router) {
+    this.loginService.getLoginDetails().subscribe(data => {
+      this.userName = data.id
+    })
+  }
+
+  ngOnInit(): void {
+    if (!this.canProceed()) {
+      alert('LoginIn to proceed Further.')
+      this.router.navigateByUrl('home')
+    }
+  }
+
+  canProceed(): boolean {
+    if (this.userName == undefined || this.userName == '')
+      return false
+    return true
+  }
 
   editMerchant(merchantId: string, i: number) {
     this.buttons[i] = false;
+  }
+
+  clearMsg() {
+    this.errorMsg = ''
   }
 
   saveMerchant(merchant: MerchantDTO, i: number) {
@@ -34,22 +58,27 @@ export class UpdateMerchantComponent implements OnInit {
     })
   }
 
-
   deleteMerchant(merchantId: string) {
-
-
     this.merchants = this.merchants.filter(merchant => {
       return merchant.merchantId != merchantId
     })
-
     alert('Merchant deleted : ' + merchantId)
+  }
 
+  getAllMerchant(){
+    
   }
 
   getMerchant() {
+    var obs: Observable<any>
     if (this.merchants != undefined)
       this.merchants = []
-    this.merchantService.getAllMerchantsByName(this.merchantName).subscribe((data: MerchantDTO[]) => {
+    if (this.merchantName == '') {
+      obs = this.merchantService.getAllMerchantsByName(this.merchantName)
+    } else {
+      obs = this.merchantService.getAllMerchantsByName(this.merchantName)
+    }
+    obs.subscribe((data: MerchantDTO[]) => {
       if (data.length == 0) {
         this.errorMsg = 'No Merchants Found';
       } else {
